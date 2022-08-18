cbd5cb87f4b965a109d920571b219590dad51b47
Mocha Dcaf <mocha@dcaf.so>
2022-08-18 18:44:48 -0400

    chore: update drip clients

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
+git pull
 openapi-generator generate -i ../drip-backend/docs/swagger.yaml -g typescript-fetch -o drip-ts
 openapi-generator generate -i ../drip-backend/docs/swagger.yaml -g go -o drip-go --additional-properties=generateInterfaces=true --additional-properties=isGoSubmodule=true --additional-properties=packageName=drip
 git restore drip-go/go.mod
