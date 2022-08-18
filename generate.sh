9a34f634f50792f6238dddf74e390d1119f2a07c
Mocha Dcaf <mocha@dcaf.so>
2022-08-18 18:45:16 -0400

    feat: make script to update clients

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
+git stash
+git checkoit main
 git pull
 openapi-generator generate -i ../drip-backend/docs/swagger.yaml -g typescript-fetch -o drip-ts
 openapi-generator generate -i ../drip-backend/docs/swagger.yaml -g go -o drip-go --additional-properties=generateInterfaces=true --additional-properties=isGoSubmodule=true --additional-properties=packageName=drip
